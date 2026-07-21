d1a2b85f0ef864b00f6c90b0ecf1b4ad58d48401
Noemi-biology <coloresciencia@gmail.com>
2026-07-21 14:57:45 -0400

    Create Clase_1.py

---
+++ Clase_1.py
@@ -0,0 +1,33 @@
+#  Todo lo que este entre "", será presentado como texto.
+print("Hello, World")
+# Guaradar textos en cajitas (variables):
+animal = "Cosmarium"
+print(animal)
+# Hacer ecuaciones:
+print(8 + 5)
+print(8 - 5)
+# Hacer ecuaciones de cosas específicas:
+corales = 8
+algas = 5
+total = corales + algas
+print(total)
+peces = 20
+salieron = 9
+total = peces - salieron
+print(total)
+# Para repetir lo mismo varias veces:
+"Coral" * 3
+"ATCGA" * 2
+# No se puede guardar 2 textos enuna misma cajita:
+planta = "Begonia"
+Planta = "Peperomia"
+print(planta)
+# La forma anterior es incorrecta, guardará solo la última en ser guardada en la cajita. Forma correcta:
+planta = "Begonia"
+print(planta)
+planta = "Peperomia"
+print(planta)
+# tener cuenta con que las ecuaciones no son textos:
+print("5" + "3")
+# Eso dará como resultado 53.
+# Vimos: Textos y variables.
