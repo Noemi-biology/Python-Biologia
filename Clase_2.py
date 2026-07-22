54faf644ba82ad152a64f35ea2f104df0c99daaa
Noemi-biology <coloresciencia@gmail.com>
2026-07-22 16:32:09 -0400

    Clase_2.py

--- Clase_2.py
+++ Clase_2.py
@@ -6,7 +6,7 @@ one = 1
 two = 2
 Three = one + two
 print(Three)
-# También funcioa con palabras (SIN OLVIDAR LAS COMILLAS):
+# También funciona con palabras (SIN OLVIDAR LAS COMILLAS):
 hello = "Hello"
 World = "World"
 helloWorld = hello + " " + World
@@ -14,10 +14,10 @@ print(helloWorld)
 # Podemos poner cosa simultaneas en la misma linea:
 a, b = 3, 4
 print(a, b)
-# Poner numeros y texto en la misma linea no trabajará.
+# Poner numeros y texto en la misma línea no trabajará.
 # Podemos poner condicionales:
 x = 1
-# SIEMPRE EN "IF" RECORDAR LOS ":"
+# SIEMPRE EN "if" RECORDAR LOS ":"
 if x == 1:
     print("x is 1")
 # Despues de un "if" se deja ese espacio o sangría.
